fdf1add4e8874b61838f9b2e7615355b5d932928
mpoluyanovich <poluyanovich@mail.ru>
2022-12-25 10:11:15 +0300

    Initial Commit

---
+++ QRcodeIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRcodeIOS
+//
+//  Created by Mikhail on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
